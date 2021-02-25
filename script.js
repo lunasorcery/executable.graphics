cf9b17e2c925bd74e4788363441f9c0a2eb18691
lunasorcery <git@moonbase.lgbt>
2021-02-25 01:00:30 +0000

    make the darkmode toggle function without localstorage

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
+// adapted from https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
 
 const toggleSwitch = document.querySelector('input[type="checkbox"]#checkbox');
-const currentTheme = localStorage.getItem('theme') ?? (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+const userDefaultTheme = (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+const currentTheme = localStorage?.getItem('theme') ?? userDefaultTheme;
 
 document.documentElement.setAttribute('data-theme', currentTheme);
 if (currentTheme === 'dark') {
@@ -11,11 +12,11 @@ if (currentTheme === 'dark') {
 function switchTheme(e) {
 	if (e.target.checked) {
 		document.documentElement.setAttribute('data-theme', 'dark');
-		localStorage.setItem('theme', 'dark');
+		localStorage?.setItem('theme', 'dark');
 	}
 	else {
 		document.documentElement.setAttribute('data-theme', 'light');
-		localStorage.setItem('theme', 'light');
+		localStorage?.setItem('theme', 'light');
 	}
 }
 
